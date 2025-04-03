f12d5a9ea79fc1823240add2eb79fd6f3f90ec99
udaykumar021 <153814010+udaykumar021@users.noreply.github.com>
2025-04-03 19:31:44 +0530

    Add files via upload

---
+++ listsandmethods.py
@@ -0,0 +1,18 @@
+"""l = [1,3,56,45]
+print(l)"""
+num = [12,13,27,34,46]
+print(num)
+"""nums = [l,num]
+print(nums)"""
+st = ['uday','ramesh','raju']
+print(st[0])
+"""num.append(87)
+print(num)"""
+"""num.insert(3.67)
+print(num)"""
+"""num.extend([25,68,99,58])
+print(num)"""
+"""num.remove(46)
+print(num)"""
+num.pop(2)
+print(num)
